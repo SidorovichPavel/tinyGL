c1eca8949fd0e6bc65b1643eb0f1bc89882c5de4
Pavel <sidorovich.pasha@gmail.com>
2021-12-11 09:38:31 +0300

    style implementation, install  commit

--- src/Style/Style.h
+++ src/Style/Style.h
@@ -1,41 +1,40 @@
 #pragma once
 
 #include <string>
-
-#define gen(type, name)	private:\
-							type name;\
-						public:\
-							type get_##name() const noexcept {return name;}\
-							void set_##name(type _Value) noexcept {name = _Value;} 
+#include <bitset>
 
 namespace tgl
 {
+	
 
 	class Style
 	{
-		gen(int, x_pos);
-		gen(int, y_pos);
-		gen(int, width);
-		gen(int, height);
-		gen(unsigned, centered);
-		gen(unsigned, visible);
-		gen(unsigned, fullScreen);
+		int mXPos, mYPos;
+		int mWidth, mHeight;
 
-		std::string title;
+		std::bitset<8> mStates;
+		std::string mTitle;
+
 	public:
+		
+		enum class State
+		{
+			Center = 0,
+			Visible,
+			Fullscreen
+		};
+
 		Style();
-		Style(std::string _Title, int _X = 0, int _Y = 0, int _W = 640, int _H = 480, unsigned _Centered = 1, unsigned _Visible = 1, unsigned _Fullscreen = 0);
+		Style(std::string&& _Title, 
+			int _X = 0, int _Y = 0, int _W = 640, int _H = 480, 
+			bool _Centered = true, bool _Visible = true, bool _Fullscreen = false);
 		~Style();
 
-		const std::string& get_title() const noexcept
-		{
-			return title;
-		}
-		void set_title(const std::string& _Title) noexcept
-		{
-			title = _Title;
-		}
-
+		const std::string& get_title() const noexcept;
+		void set_title(const std::string& _Title) noexcept;
+		void set_state(State _State, bool _Val = true);
+		bool get_state(State _State) const;
+		std::pair<int, int> get_size() const noexcept;
 	};
 
 }
--- src/Style/style.cpp
+++ src/Style/style.cpp
@@ -4,27 +4,54 @@ namespace tgl
 {
 	Style::Style()
 		:
-		x_pos(0),
-		y_pos(0),
-		width(0),
-		height(0),
-		centered(0),
-		fullScreen(0),
-		visible(0)
-	{}
-	Style::Style(std::string _Title, int _X, int _Y, int _W, int _H, unsigned _Centered, unsigned _Visible, unsigned _Fullscreen)
+		mXPos(0),
+		mYPos(0),
+		mWidth(0),
+		mHeight(0)
+	{
+		mStates.reset();
+	}
+
+	Style::Style(std::string&& _Title,
+		int _X, int _Y, int _W, int _H,
+		bool _Centered, bool _Visible, bool _Fullscreen)
 		:
-		x_pos(_X),
-		y_pos(_Y),
-		width(_W),
-		height(_H),
-		centered(_Centered),
-		visible(_Visible),
-		fullScreen(_Fullscreen),
-		title(std::move(_Title))
-	{}
-
-
+		mXPos(_X),
+		mYPos(_Y),
+		mWidth(_W),
+		mHeight(_H),
+		mTitle(std::move(_Title))
+	{
+		mStates[static_cast<size_t>(State::Center)]		= _Centered;
+		mStates[static_cast<size_t>(State::Visible)]	= _Centered;
+		mStates[static_cast<size_t>(State::Fullscreen)] = _Centered;
+	}
 
 	Style::~Style() {};
+
+	const std::string& Style::get_title() const noexcept
+	{
+		return mTitle;
+	}
+
+	void Style::set_title(const std::string& _Title) noexcept
+	{
+		mTitle = _Title;
+	}
+
+	void Style::set_state(State _State, bool _Val)
+	{
+		mStates[static_cast<size_t>(_State)] = _Val;
+	}
+
+	bool Style::get_state(State _State) const
+	{
+		return mStates[static_cast<size_t>(_State)];
+	}
+
+	std::pair<int, int> Style::get_size() const noexcept
+	{
+		return std::pair<int, int>(mWidth, mHeight);
+	}
+
 }
