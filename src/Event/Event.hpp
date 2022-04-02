cbfe5037d95f2cfbb4f5e1849cbf80990e4f5f75
Pavel <sidorovich.pasha@gmail.com>
2022-04-03 00:06:28 +0300

    transfer on pointers

--- src/Event/Event.hpp
+++ src/Event/Event.hpp
@@ -8,66 +8,73 @@
 
 namespace tgl
 {
-	template<class T>
-	struct ActualType
+	namespace detail
 	{
-		using type = T;
-	};
-
-	template<class Ret, class... Args>
-	class IConteiner
-	{
-	public:
-		IConteiner() {};
-		virtual Ret call(Args...) = 0;
-	};
-
-	template<class Class, class Ret, class... Args>
-	class ObjConteiner final : public IConteiner<Ret, Args...>
-	{
-		using Method = typename ActualType<Ret(Class::*)(Args...)>::type;
-		Class* mClassPtr;
-		Method mMethod;
-
-	public:
-
-		ObjConteiner(Class* _Ptr, Method _Met) noexcept
-			:
-			mClassPtr(_Ptr),
-			mMethod(_Met)
-		{};
-
-		Ret call(Args... args)
+		template<class T>
+		struct ActualType
 		{
-			if constexpr (std::is_same_v<Ret, void>)
-				(mClassPtr->*mMethod)(args...);
-			else
-				return (mClassPtr->*mMethod)(args...);
-		}
-	};
+			using type = T;
+		};
 
-	template<class Ret, class... Args>
-	class FuncConteiner final : public IConteiner<Ret, Args...>
-	{
-		using func_t = ActualType<Ret(__cdecl*)(Args...)>::type;
-		func_t mFunction;
-
-	public:
-
-		/*pleas, see youre call type*/
-		FuncConteiner(func_t _Fn) 
-			:
-			mFunction(_Fn) 
-		{};
-
-		Ret call(Args... args)
+		template<class Ret, class... Args>
+		class IConteiner
 		{
-			if constexpr (std::is_same_v<Ret, void>)
-				mFunction(std::forward<Args>(args)...);
-			else
-				return mFunction(std::forward<Args>(args)...);
-		}
-	};
+		public:
+			IConteiner() {};
+			virtual ~IConteiner() {};
+			virtual Ret call(Args...) = 0;
+		};
+
+
+
+		template<class Class, class Ret, class... Args>
+		class ObjConteiner final : public IConteiner<Ret, Args...>
+		{
+			using Method = typename ActualType<Ret(Class::*)(Args...)>::type;
+			Class* mClassPtr;
+			Method mMethod;
+
+		public:
+
+			ObjConteiner(Class* _Ptr, Method _Met) noexcept
+				:
+				mClassPtr(_Ptr),
+				mMethod(_Met)
+			{};
+
+			Ret call(Args... args)
+			{
+				if constexpr (std::is_same_v<Ret, void>)
+					(mClassPtr->*mMethod)(args...);
+				else
+					return (mClassPtr->*mMethod)(args...);
+			}
+		};
+
+		template<class Ret, class... Args>
+		class FuncConteiner final : public IConteiner<Ret, Args...>
+		{
+			using func_t = ActualType<Ret(__cdecl*)(Args...)>::type;
+			func_t mFunction;
+
+		public:
+
+			/*pleas, see youre call type*/
+			FuncConteiner(func_t _Fn)
+				:
+				mFunction(_Fn)
+			{};
+
+			Ret call(Args... args)
+			{
+				if constexpr (std::is_same_v<Ret, void>)
+					mFunction(std::forward<Args>(args)...);
+				else
+					return mFunction(std::forward<Args>(args)...);
+			}
+		};
+
+	}
 
 	template<class T>
 	class Event
@@ -83,26 +90,35 @@ namespace tgl
 	{
 	public:
 		static_assert("No... Pleas, use that: Event<return_t(arguments_t...)>");
-		using id_type = int;
+		using id_type = void*;
 	};
 
-	template<class Ret, class... Args>
-	class Event<Ret(Args...)>
+	template<class Ret, class... Params>
+	class Event<Ret(Params...)>
 	{
 	public:
-		using IConteiner = IConteiner<Ret, Args...>;
-		using function_type = ActualType<Ret(*)(Args...)>::type;
-		using func_conteiner_type = FuncConteiner<Ret, Args...>; //sizeof(func_conteiner_type) == 8;
+		using IConteiner = detail::IConteiner<Ret, Params...>;
+		using function_type = detail::ActualType<Ret(*)(Params...)>::type;
+		using func_conteiner_type = detail::FuncConteiner<Ret, Params...>; //sizeof(func_conteiner_type) == 8;
 
 		template<class ClassT>
-		using method_type = ActualType<Ret(ClassT::*)(Args...)>::type;
+		using method_type = detail::ActualType<Ret(ClassT::*)(Params...)>::type;
 
 		template<class ClassT>
-		using obj_conteiner_type = ObjConteiner<ClassT, Ret, Args...>; //sizeof(obj_conteiner_type) == 12;
+		using obj_conteiner_type = detail::ObjConteiner<ClassT, Ret, Params...>; //sizeof(obj_conteiner_type) == 12;
 
-		using id_type = int;
+		using id_type = void*;
 
-		Event() {}
+		Event()
+		{
+			zero_data();
+		}
+
+		~Event()
+		{
+			detach_all();
+		}
+
 		Event(const Event&) = delete;
 		Event& operator=(const Event&) = delete;
 		Event(Event&&) = delete;
@@ -112,60 +128,75 @@ namespace tgl
 		[[nodiscard]]
 		id_type attach(ClassT* _Ptr, method_type<ClassT> _Met)
 		{
-			auto [id, place] = get_place();
-			if (place == nullptr)
-				return id;
-			
-			new(place) obj_conteiner_type<ClassT>(_Ptr, _Met);
-			return id;
+			auto place = get_place();
+			if (is_valid(place))
+				new(place) obj_conteiner_type<ClassT>(_Ptr, _Met);
+
+			return place;
 		}
 
 		[[nodiscard]]
 		id_type attach(function_type _Fn)
 		{
-			auto [id, place] = get_place();
-			if (place == nullptr)
-				return id;
+			auto place = get_place();
+			if (is_valid(place))
+				new(place) func_conteiner_type(_Fn);
 
-			new(place) func_conteiner_type(_Fn);
-			return id;
+			return place;
 		}
 
 		void detach(id_type _Id)
 		{
-			mInvokeMask ^= _Id;
+			auto place = static_cast<uint8_t*>(_Id);
+			if (is_valid(place))
+			{
+				auto obj = static_cast<IConteiner*>(_Id);
+				obj->~IConteiner();
+				std::fill(place, place + mObjSize, 0ui8);
+			}
 		}
 
 		void detach_all()
 		{
-			mInvokeMask = 0;
+			auto [begin, end] = get_data_range();
+			for (auto place = begin; place != end; place += mObjSize)
+				if (*place)
+				{
+					auto obj = reinterpret_cast<IConteiner*>(place);
+					obj->~IConteiner();
+				}
+			zero_data();
 		}
 
-		auto operator()(Args... args)
+		template<class... Args>
+		auto operator()(Args&&... args)
 		{
 			if constexpr (std::is_same_v<Ret, void>)
 			{
-				for (auto idx = 0; idx < mMaxObjNumber; idx++)
+				auto [begin, end] = get_data_range();
+				for (auto place = begin; place != end; place += mObjSize)
 				{
-					int mask = 1 << idx;
-					if (mask & mInvokeMask)
-					{
-						auto elem = reinterpret_cast<IConteiner*>(mInvokeData.data() + idx * mObjSize);
-						elem->call(args...);
-					}
+					auto test_place = reinterpret_cast<size_t*>(place);
+					if (!*test_place)
+						continue;
+
+					auto obj = reinterpret_cast<IConteiner*>(place);
+					obj->call(std::forward<Args>(args)...);
 				}
 			}
 			else
 			{
 				std::vector<Ret> results;
-				for (auto idx = 0; idx < mMaxObjNumber; idx++)
+				auto [begin, end] = get_data_range();
+				for (auto place = begin; place != end; place += mObjSize)
 				{
-					int mask = 1 << idx;
-					if (mask & mInvokeMask)
-					{
-						auto elem = reinterpret_cast<IConteiner*>(mInvokeData.data() + idx * mObjSize);
-						results.push_back(elem->call(args...));
-					}
+					auto test_place = reinterpret_cast<size_t*>(place);
+					if (!*test_place)
+						continue;
+
+					auto obj = reinterpret_cast<IConteiner*>(place);
+					results.push_back(obj->call(std::forward<Args>(args)...));
+
 				}
 				return results;
 			}
@@ -173,30 +204,40 @@ namespace tgl
 
 		void swap(Event& _Other) noexcept
 		{
-			std::swap(mInvokeMask, _Other.mInvokeMask);
 			std::swap(mInvokeData, _Other.mInvokeData);
 		}
 
 	private:
 
-		std::pair<int, uint8_t*> get_place() noexcept
+		uint8_t* get_place() noexcept
 		{
-			for (int idx = 0; idx < mMaxObjNumber; idx++)
-			{
-				int mask = 1 << idx;
-				if (!(mask & mInvokeMask))
-				{
-					mInvokeMask |= mask;
-					return { mask, mInvokeData.data() + idx * mObjSize };
-				}
-			}
-			return { -1, nullptr };
+			auto [begin, end] = get_data_range();
+			for (auto place = begin; place != end; place += mObjSize)
+				if (!*place)
+					return place;
+
+			return end;
 		}
 
-		int mInvokeMask;
-		static constexpr auto mObjSize = sizeof(size_t) * 3;
+		std::pair<uint8_t*, uint8_t*> get_data_range() noexcept
+		{
+			return { mInvokeData.data(), mInvokeData.data() + mInvokeData.size() };
+		}
+
+		bool is_valid(uint8_t* _Place) noexcept
+		{
+			auto [begin, end] = get_data_range();
+			return _Place >= begin && _Place < end;
+		}
+
+		void zero_data() noexcept
+		{
+			std::fill(mInvokeData.begin(), mInvokeData.end(), 0ui8);
+		}
+
+		static constexpr auto mObjSize = sizeof(size_t) * 3;	// hack. i think about it
 		static constexpr auto mMaxObjNumber = 5;
-		static constexpr auto mMaxMaskValue = 1 << mMaxObjNumber;
+
 		std::array<uint8_t, (mMaxObjNumber* mObjSize)> mInvokeData;
 	};
 }
